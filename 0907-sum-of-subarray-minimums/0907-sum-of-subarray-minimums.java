2e737a940e95cd0beebfc60ad14a8eaf7d3af659
ADITYA RAJ <adityasharma14807@gmail.com>
2026-08-15 01:47:41 +0530

    Time: 298 ms (32.05%), Space: 110.7 MB (19.41%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        long mod = 1000000007;
+        int[] pse = new int[n];
+        int[] nse = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i =0;i<n;i++){
+            while(st.size()>0 && arr[i] < arr[st.peek()]){
+                st.pop();
+            }
+            if(st.size()==0) pse[i] = -1;
+            else pse[i] = st.peek();
+            st.push(i);
+        }
+        st.clear(); // isse pura stack clear ho jata hai
+
+        for(int i = n-1;i>=0;i--){
+            while(st.size()>0 && arr[i]<=arr[st.peek()]){
+                st.pop();
+            }
+            if(st.size()==0) nse[i] = n;
+            else nse[i] = st.peek();
+            st.push(i);
+        }
+         long ans = 0;
+         for(int i =0;i<n;i++){
+            long leftcount = i - pse[i];
+            long rightcount = nse[i] - i;
+
+            ans += arr[i] * leftcount * rightcount;
+            ans %= mod;
+        }
+        return (int)ans;
+    }
+}
