5862ba2387d8a4ed780343a42458226a1419a85e
ADITYA RAJ <adityasharma14807@gmail.com>
2026-08-15 23:37:22 +0530

    Time: 4 ms (83.06%), Space: 46.7 MB (83.68%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,31 +1,28 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> st = new Stack<>();
-
         for(int arr : asteroids){
             boolean destroyed = false;
-            while(st.size()>0 && st.peek()>0 && arr<0){
-                if(st.peek() < -arr){
+            while(st.size()>0 && arr < 0 && st.peek() > 0){
+                if(st.peek()< -arr){
                     st.pop();
-                }
-                else if(st.peek() == -arr){
+                }else if(st.peek()==-arr){
                     st.pop();
                     destroyed = true;
                     break;
-                }
-                else{
+                }else{
                     destroyed = true;
                     break;
                 }
             }
             if(!destroyed){
-                    st.push(arr);
+                st.push(arr);
             }
         }
-        int[] adi = new int[st.size()];
-        for(int i=0;i<adi.length;i++){
-            adi[i] = st.get(i);
+        int[] res = new int[st.size()];
+        for(int i =0;i<st.size();i++){
+            res[i] = st.get(i);
         }
-        return adi;
+        return res;
     }
 }
