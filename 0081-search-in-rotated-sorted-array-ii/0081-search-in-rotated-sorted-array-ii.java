f42625df8938e49e86fb314f772c4ad091a239c0
ADITYA RAJ <adityasharma14807@gmail.com>
2026-08-18 22:59:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -4,25 +4,25 @@ class Solution {
         int low = 0;
         int high = n-1;
         while(low<=high){
-            int mid = low + (high-low)/2;
+            int mid = low+(high-low)/2;
             if(arr[mid]==target) return true;
-            if(arr[low]==arr[mid] && arr[mid]==arr[high]){
+            if(arr[mid] == arr[low] && arr[mid] == arr[high]){
                 low++;
                 high--;
-            }
-           else if(arr[low]<= arr[mid]){ // agar aisa hai then left half sorted hai
-                if(arr[mid]>target && arr[low]<=target){
+            }    
+            else if(arr[low]<=arr[mid]){
+                if(arr[low]>=target && arr[mid]<=target){
                     high = mid-1;
-                }else{
+                } else{
                     low = mid+1;
                 }
             }
-            else{ // Right part sorted hai
-            if(arr[mid]<=target && arr[high]>=target){
-                low = mid+1;
-            }else{
-                high = mid-1;
-            }
+            else{
+                if(arr[mid]<=target && arr[high]>=target){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
             }
         }
         return false;
