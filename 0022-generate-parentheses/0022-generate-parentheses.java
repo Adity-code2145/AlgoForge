5bfa6b52dafce7c033cdefad9e643ef24ca9930f
ADITYA RAJ <adityasharma14807@gmail.com>
2026-08-19 16:58:02 +0530

    Time: 2 ms (70%), Space: 44.6 MB (65.19%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        print(n,0,0,"",ans);
+        return ans;
+    }
+    public void print(int n, int l, int r, String s, List<String> ans){
+        if(r == n){
+            ans.add(s);
+            return;
+        }
+        if(l<n) print(n, l+1, r, s+'(',ans);
+        if(l>r) print(n, l,r +1 , s+')', ans);
+    }
+}
