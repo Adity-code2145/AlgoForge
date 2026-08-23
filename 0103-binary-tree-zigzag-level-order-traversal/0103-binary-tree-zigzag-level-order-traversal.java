2842554d87603f9b9e3d33e0fbb708e1a331f793
ADITYA RAJ <adityasharma14807@gmail.com>
2026-08-23 15:48:17 +0530

    Time: 1 ms (80.87%), Space: 43.9 MB (9.87%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,42 +1,27 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        boolean leftToRight = true;
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        boolean lefttoright = true;
+        while(!q.isEmpty()){
+            int size = q.size();
             LinkedList<Integer> level = new LinkedList<>();
+            for(int i =0;i<size;i++){
+                TreeNode node = q.poll();
 
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-
-                if (leftToRight) {
+                if(lefttoright){
                     level.addLast(node.val);
-                } else {
+                }else{
                     level.addFirst(node.val);
                 }
-
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
+                if(node.left!=null) q.offer(node.left);
+                if(node.right!=null) q.offer(node.right);
             }
-
-            result.add(level);
-            leftToRight = !leftToRight;
+            ans.add(level);
+            lefttoright = !lefttoright;
         }
-
-        return result;
+        return ans;
     }
 }
