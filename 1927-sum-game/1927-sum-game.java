1a5e0adb50e1991f2a8accd4ee812b2a48003a16
ADITYA RAJ <adityasharma14807@gmail.com>
2026-08-23 12:06:46 +0530

    Time: 7 ms (79.56%), Space: 47.2 MB (10.95%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean sumGame(String num) {
+
+        int n = num.length();
+        int mid = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < mid; i++) {
+            if (num.charAt(i) == '?') {
+                leftQ++;
+            } else {
+                leftSum += num.charAt(i) - '0';
+            }
+        }
+
+        for (int i = mid; i < n; i++) {
+            if (num.charAt(i) == '?') {
+                rightQ++;
+            } else {
+                rightSum += num.charAt(i) - '0';
+            }
+        }
+
+        // Odd number of '?' -> Alice wins
+        if ((leftQ + rightQ) % 2 == 1) {
+            return true;
+        }
+
+        // Bob can make both sums equal
+        int required = (rightQ - leftQ) / 2 * 9;
+
+        if (leftSum - rightSum == required) {
+            return false;
+        }
+
+        return true;
+    }
+}
