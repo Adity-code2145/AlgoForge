2cfdc553107b0fbb458bf866adb43c9dea38e9ec
ADITYA RAJ <adityasharma14807@gmail.com>
2026-08-25 22:00:36 +0530

    Time: 1 ms (70.45%), Space: 43.6 MB (69.54%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,17 +1,20 @@
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        view(root,0,ans);
+        if(root == null){
+            return ans;
+        }
+        level(root,0,ans);
         return ans;
     }
-    public void view(TreeNode root, int level, List<Integer> ans){
+    public void level(TreeNode root, int level, List<Integer> ans){
         if(root == null) return;
-        if(level >= ans.size()){
+        if(ans.size()<=level){
             ans.add(root.val);
         }else{
             ans.set(level,root.val);
         }
-        view(root.left,level+1,ans);
-        view(root.right, level+1,ans);
+        level(root.left, level+1, ans);
+        level(root.right, level+1,ans);
     }
 }
