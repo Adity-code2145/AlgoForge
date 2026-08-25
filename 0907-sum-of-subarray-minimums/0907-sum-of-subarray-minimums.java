95e3139f761574fcb5d0e7bc819f2fbc3ab1e946
ADITYA RAJ <adityasharma14807@gmail.com>
2026-08-25 14:02:19 +0530

    Time: 288 ms (51.43%), Space: 110.3 MB (28.99%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,73 +1,35 @@
-// class Solution {
-//     public int sumSubarrayMins(int[] arr) {
-//         int n = arr.length;
-//         long mod = 1000000007;
-//         Stack<Integer> st = new Stack<>();
-//         int[] pse = new int[n];
-//         int[] nse = new int[n];
-
-//         for(int i =0;i<n;i++){
-//             while(st.size()>0 && arr[st.peek()]>arr[i]){
-//                 st.pop();
-//             }
-//             if(st.size()==0) pse[i] = -1;
-//             else pse[i] = st.peek();
-//             st.push(i);
-//         }
-
-//         st.clear();
-
-//         for(int i =n-1;i>=0;i--){
-//             while(st.size()>0 && arr[st.peek()]>=arr[i]){
-//                 st.pop();
-//             }
-//             if(st.size()==0) nse[i] = n;
-//             else nse[i] = st.peek();
-//             st.push(i);
-//         }
-//         long ans = 0;
-//         for(int i =0;i<n;i++){
-//             int leftcount = i - pse[i];
-//             int rightcount = nse[i] -i;
-//             ans += arr[i] *leftcount*rightcount;
-//             ans %= mod;
-//         }
-//         return (int)ans;
-//     }
-// }
-
 class Solution {
     public int sumSubarrayMins(int[] arr) {
         int n = arr.length;
         long mod = 1000000007;
+        
         int[] pse = new int[n];
         int[] nse = new int[n];
         Stack<Integer> st = new Stack<>();
-
         for(int i =0;i<n;i++){
-            while(st.size()>0 && arr[i] < arr[st.peek()]){
+            while(st.size()>0 && arr[st.peek()]>arr[i]){
                 st.pop();
             }
             if(st.size()==0) pse[i] = -1;
             else pse[i] = st.peek();
             st.push(i);
         }
-        st.clear(); // isse pura stack clear ho jata hai
 
-        for(int i = n-1;i>=0;i--){
-            while(st.size()>0 && arr[i]<=arr[st.peek()]){
+        st.clear();
+
+        for(int i =n-1;i>=0;i--){
+            while(st.size()>0 && arr[st.peek()]>=arr[i]){
                 st.pop();
             }
             if(st.size()==0) nse[i] = n;
             else nse[i] = st.peek();
             st.push(i);
         }
-         long ans = 0;
-         for(int i =0;i<n;i++){
+        long ans = 0;
+        for(int i =0;i<n;i++){
             long leftcount = i - pse[i];
-            long rightcount = nse[i] - i;
-
-            ans += arr[i] * leftcount * rightcount;
+            long rightcount = nse[i] -i;
+            ans += arr[i] *leftcount*rightcount;
             ans %= mod;
         }
         return (int)ans;
