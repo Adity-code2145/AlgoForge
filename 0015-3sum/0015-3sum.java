3a8408a30b41e0bb91c036094cc731f662b8f048
ADITYA RAJ <adityasharma14807@gmail.com>
2026-09-02 23:39:07 +0530

    Time: 34 ms (59.17%), Space: 59.3 MB (32.1%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -11,22 +11,20 @@ class Solution {
             int right = n-1;
             while(left<right){
                 int sum = arr[i] + arr[left] + arr[right];
-                if(sum<0){
-                    left++;
-                }
-                else if(sum>0){
-                    right--;
-                }
-                else{
-                    ans.add(Arrays.asList(arr[i],arr[left],arr[right]));
+                if (sum == 0) {
+                    ans.add(Arrays.asList(arr[i], arr[left], arr[right]));
+
+                    while (left < right && arr[left] == arr[left + 1]) left++;
+                    while (left < right && arr[right] == arr[right - 1]) right--;
+
                     left++;
                     right--;
-                    while(left<right && arr[left] == arr[left-1]){
-                        left++;
-                    }
-                    while(left<right && arr[right] == arr[right+1]){
-                        right--;
-                    }
+                }
+                else if (sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
                 }
             }
         }
