30eb0694fab9c93c9e42c379b45db945baa0b1cf
ADITYA RAJ <adityasharma14807@gmail.com>
2026-09-02 11:44:31 +0530

    Time: 450 ms (22.08%), Space: 271.6 MB (28.04%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -3,10 +3,8 @@ class Solution {
         List<String> ans = new ArrayList<>();
         HashSet<String> seen = new HashSet<>();
         HashSet<String> rep = new HashSet<>();
-
-        for(int i =0;i<=s.length()-10;i++){
+        for(int i=0;i<=s.length()-10;i++){
             String str = s.substring(i,i+10);
-
             if(seen.contains(str)){
                 rep.add(str);
             }
