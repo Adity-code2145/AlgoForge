ea6bc6f8a21d916d82f7f94556a56dfc9f4d96b7
ADITYA RAJ <adityasharma14807@gmail.com>
2026-09-02 10:46:12 +0530

    Time: 0 ms (100%), Space: 43.7 MB (44.09%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -4,22 +4,23 @@ class Solution {
         if(root == null) return ans;
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
-        boolean lefttoright = true;
+        boolean flag = true;
         while(!q.isEmpty()){
-            int size = q.size();
             LinkedList<Integer> level = new LinkedList<>();
-            for(int i=0;i<size;i++){
-                TreeNode node = q.remove();
-                if(lefttoright){
+            int size = q.size();
+            for(int i =0;i<size;i++){
+                TreeNode node = q.poll();
+                if(flag){
                     level.addLast(node.val);
-                }else{
+                }
+                else{
                     level.addFirst(node.val);
                 }
-                if(node.left != null) q.add(node.left);
-                if(node.right!=null)  q.add(node.right);
+                if(node.left!=null) q.offer(node.left);
+                if(node.right!=null) q.offer(node.right);
             }
             ans.add(level);
-            lefttoright = !lefttoright;
+            flag = !flag;
         }
         return ans;
     }
