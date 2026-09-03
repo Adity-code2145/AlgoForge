267f21b92df097bc05e97be02eb7eec50c6f7a85
ADITYA RAJ <adityasharma14807@gmail.com>
2026-09-03 14:25:20 +0530

    Time: 20 ms (30.97%), Space: 98.3 MB (5.17%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -5,14 +5,13 @@ class Solution {
         print(1,n,k,ans,temp);
         return ans;
     }
-    public void print(int start, int n, int k, List<List<Integer>> ans,List<Integer> temp){
-        if(temp.size() == k){
+    public void print(int start, int n, int k, List<List<Integer>> ans, List<Integer> temp){
+        if(temp.size()==k){
             ans.add(new ArrayList<>(temp));
             return;
         }
-        for(int i =start;i<=n;i++){
+        for(int i = start;i<=n;i++){
             temp.add(i);
-
             print(i+1,n,k,ans,temp);
             temp.remove(temp.size()-1);
         }
