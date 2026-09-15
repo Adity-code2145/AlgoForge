904d98e52bbda7970df8bf6937664cbda5a9256b
ADITYA RAJ <adityasharma14807@gmail.com>
2026-09-15 13:58:56 +0530

    Time: 0 ms (100%), Space: 47 MB (87.6%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -5,18 +5,18 @@ class Solution {
         boolean[] vis = new boolean[n];
         for(int i =0;i<n;i++){
             if(!vis[i]){
-                dfs(i,adj,vis);
+                dfs(i,vis,adj);
                 count++;
             }
         }
         return count;
     }
-    public void dfs(int i, int[][] adj, boolean[] vis){
+    public void dfs(int i, boolean[] vis, int[][] adj){
         int n = adj.length;
         vis[i] = true;
         for(int j =0;j<n;j++){
             if(adj[i][j] == 1 && vis[j] == false){
-                dfs(j,adj,vis);
+                dfs(j,vis,adj);
             }
         }
     }
