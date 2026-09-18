154dcae6e344b623d6ff759f921d54f54f7f9493
ADITYA RAJ <adityasharma14807@gmail.com>
2026-09-18 11:41:49 +0530

    Time: 5 ms (80.56%), Space: 46.8 MB (52.63%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,39 +1,36 @@
 class Solution {
-    public int[] findOrder(int n, int[][] mat) {
+    public int[] findOrder(int n, int[][] pre) {
         List<List<Integer>> adj = new ArrayList<>();
         for(int i =0;i<n;i++){
             adj.add(new ArrayList<>());
         }
         int[] indegree = new int[n];
-        for(int i = 0;i<mat.length;i++){
-            int a = mat[i][0];
-            int b = mat[i][1];
+        for(int i =0;i<pre.length;i++){
+            int a = pre[i][0];
+            int b = pre[i][1];
             adj.get(b).add(a);
             indegree[a]++;
         }
         Queue<Integer> q = new LinkedList<>();
-        
+        int[] ans = new int[n];
+        int idx = 0;
         for(int i =0;i<n;i++){
             if(indegree[i] == 0){
                 q.add(i);
             }
         }
-        int[] ans = new int[n];
-        int idx = 0;
         while(!q.isEmpty()){
             int front = q.remove();
             ans[idx] = front;
             idx++;
             for(int ele : adj.get(front)){
                 indegree[ele]--;
-                if(indegree[ele] == 0){
+                if(indegree[ele]==0){
                     q.add(ele);
                 }
             }
         }
-        if(idx != n){
-            return new int[0];
-        }
+        if(n!=idx) return new int[0];
         return ans;
     }
 }
