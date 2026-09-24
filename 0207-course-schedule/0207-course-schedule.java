0363faacd15678055764dd1ad25e7b8b0ef8231a
ADITYA RAJ <adityasharma14807@gmail.com>
2026-09-24 11:34:39 +0530

    Time: 8 ms (25.01%), Space: 47 MB (64.06%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,36 +1,33 @@
 class Solution {
-    static boolean ans;
     public boolean canFinish(int n, int[][] pre) {
-        ans = true; // if there is no cycle
         List<List<Integer>> adj = new ArrayList<>();
         for(int i =0;i<n;i++){
             adj.add(new ArrayList<>());
         }
+        int[] indegree = new int[n];
         for(int i =0;i<pre.length;i++){
             int a = pre[i][0];
             int b = pre[i][1];
-            adj.get(b).add(a);
-           // adj.get(a).add(b);
+            adj.get(a).add(b);
+            indegree[b]++;
         }
-        boolean[] path = new boolean[n];
-        boolean[] vis = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        int count =0;
         for(int i =0;i<n;i++){
-            if(vis[i]== false){
-                dfs(i,vis,path,adj);
+            if(indegree[i] == 0){
+                q.add(i);
             }
         }
-        return ans;
-    }
-    public void dfs(int i, boolean[] vis, boolean[] path,List<List<Integer>> adj){
-        vis[i] = true;
-        path[i] = true;
-        for(int ele : adj.get(i)){
-            if(path[ele] == true){ // cycle detect
-                ans = false;
-                return;
+        while(!q.isEmpty()){
+            int front = q.remove();
+            count++;
+            for(int ele : adj.get(front)){
+                indegree[ele]--;
+                if(indegree[ele] == 0){
+                    q.add(ele);
+                }
             }
-            if(vis[ele]== false) dfs(ele,vis,path,adj);
         }
-        path[i] = false;
+        return n == count;
     }
 }
